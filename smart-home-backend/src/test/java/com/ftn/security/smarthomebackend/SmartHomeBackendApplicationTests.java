a4cf1531802dfa2037770dfe7062ea38e57db83d
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2023-03-23 15:43:41 +0100

    Backend app initialized

---
+++ smart-home-backend/src/test/java/com/ftn/security/smarthomebackend/SmartHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.security.smarthomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
