f61b2bf35f842a641997b9be7469874354d9dde9
sdjuric00 <103071405+sdjuric00@users.noreply.github.com>
2023-04-03 13:45:20 +0200

    Adding intermediate type to the front end

--- smart-home-backend/src/main/resources/data-postgres.sql
+++ smart-home-backend/src/main/resources/data-postgres.sql
@@ -4,8 +4,8 @@ insert into regular_user (id, email, password, name, surname, salt, status, fail
 insert into admin (id, email, password, name, surname, salt, status, failed_attempts, locked_until, verified, role) values
     (nextval('users_id_gen'), 'admin@gmail.com', '$2a$10$8TWonhaYGbjZ1C69pQwB0uWBOANl1FCwz0wxH9z2LsKXIhTM1hUay', 'Admin', 'Admin', '123', 0, 0, null, true, 2);
 
- INSERT INTO csr (user_id, common_name, organization_unit, organization, city, state, country, status) VALUES
-     (1, 'www.example.com', 'IT', 'Example Company', 'San Francisco', 'CA', 'US', 0);
+--  INSERT INTO csr (user_id, common_name, organization_unit, organization, city, state, country, status) VALUES
+--      (1, 'www.example.com', 'IT', 'Example Company', 'San Francisco', 'CA', 'US', 0);
 
 -- INSERT INTO cancel_certificate(alias, reason, most_recent) values
 --     ('peki@gmail.com', 'neki razlog', true);
